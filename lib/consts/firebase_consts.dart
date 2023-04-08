78083ed73e956e17e7c9569d26fa05459e52d1d6
jsagayap <sagayapjericho@gmail.com>
2023-04-09 03:09:37 +0800

    chore: add collections to firebase constants

--- lib/consts/firebase_consts.dart
+++ lib/consts/firebase_consts.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+const usersCollection = 'users';
+const appointmentsCollection = 'appointments';
+const schedulesCollection = 'schedules';
 const productsCollection = 'products';
 const cartCollection = 'cart';
 const ordersCollection = 'orders';

4c10dcf5f197c8f21c7c8651e1b35302accc7f14
aristoleBuenaventura <aristotlecortezbuenaventura@gmail.com>
2023-05-16 00:32:08 +0800

    sorted the ongoing and completed appointments

--- lib/screens/patient_list/patient_list_screen.dart
+++ lib/screens/patient_list/patient_list_screen.dart
@@ -75,49 +75,67 @@ class ListPatientsByStatus extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(20),
         child: FutureBuilder(
-          future: FirestoreServices.getAppointments(userId, status),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
+            future: FirestoreServices.getAppointments(userId, status),
+            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              else if (snapshot.data!.docs.isEmpty) {
+                return const ItemIndicator(
+                  icon: IconlyBroken.calendar,
+                  text: 'No appointments found',
+                );
+              }
+              else {
+                var data = snapshot.data!.docs;
+
+                // Sort the appointments by date and time
+                data.sort((a, b) {
+                  Map<String, dynamic> appointmentA = a.data() as Map<String, dynamic>;
+                  Map<String, dynamic> appointmentB = b.data() as Map<String, dynamic>;
+
+                  int compareMonth = appointmentA['month'].compareTo(appointmentB['month']);
+                  if (compareMonth != 0) {
+                    return compareMonth;
+                  }
+
+                  int compareDay = appointmentA['day'].compareTo(appointmentB['day']);
+                  if (compareDay != 0) {
+                    return compareDay;
+                  }
+
+                  return appointmentA['time'].compareTo(appointmentB['time']);
+                });
+
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    DocumentSnapshot appointmentTmp = data[index];
+                    Map<String, dynamic> appointment = appointmentTmp.data() as Map<String, dynamic>;
+                    String appointmentId = data[index].id;
+
+                    return Column(
+                      children: [
+                        PersonCard(
+                          name: '',
+                          customName: GetPatientName(documentId: appointment['patientId']),
+                          description: '${appointment['month']} ${appointment['day']}',
+                          subtext: appointment['time'],
+                          imageUrl: appointment.containsKey('imageUrl') ? appointment['imageUrl'] : defAvatar,
+                          customImage: GetPatientImage(documentId: appointment['patientId']),
+                          action: () => Get.to(() => AppointmentDetailsDoctorScreen(
+                            appointmentId: appointmentId,
+                          )),
+                        ),
+                        const SizedBox(height: 12),
+                      ],
+                    );
+                  },
+                );
+              }
             }
-            else if (snapshot.data!.docs.isEmpty) {
-              return const ItemIndicator(
-                icon: IconlyBroken.calendar,
-                text: 'No appointments found',
-              );
-            }
-            else {
-              var data = snapshot.data!.docs;
-    
-              return ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  DocumentSnapshot appointmentTmp = data[index];
-                  Map<String, dynamic> appointment = appointmentTmp.data() as Map<String, dynamic>;
-                  String appointmentId = data[index].id;
-    
-                  return Column(
-                    children: [
-                      PersonCard(
-                        name: '',
-                        customName: GetPatientName(documentId: appointment['patientId']),
-                        description: '${appointment['month']} ${appointment['day']}',
-                        subtext: appointment['time'],
-                        imageUrl: appointment.containsKey('imageUrl') ? appointment['imageUrl'] : defAvatar,
-                        customImage: GetPatientImage(documentId: appointment['patientId']),
-                        action: () => Get.to(() => AppointmentDetailsDoctorScreen(
-                          appointmentId: appointmentId,
-                        )),
-                      ),
-                      const SizedBox(height: 12),
-                    ],
-                  );
-                },
-              );
-            }
-          }
         ),
       ),
     );
